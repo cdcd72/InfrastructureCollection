d583446f6f6230208ce199394d36ef35e97e6133
cdcd72 <cdcd71517@gmail.com>
2021-11-25 15:44:50 +0800

    [Infra.FileAccess.Grpc] Link GrpcFileServer proto files

--- src/Infra/FileAccess/Infra.FileAccess.Grpc/GrpcFileAccess.cs
+++ src/Infra/FileAccess/Infra.FileAccess.Grpc/GrpcFileAccess.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
+using GrpcFileServer;
 using Infra.Core.FileAccess.Abstractions;
 using Infra.Core.FileAccess.Models;
 using Infra.FileAccess.Grpc.Common;
