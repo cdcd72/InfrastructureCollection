eea9c290f7f5c16ff1c0532cc1d0c4681f5a34e9
cdcd72 <cdcd71517@gmail.com>
2022-07-07 13:57:39 +0800

    [Infra.Core] Add RandomNumberGenerator

---
+++ src/Core/Infra.Core/Utility/RandomNumberGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Infra.Core.Utility;
+
+public static class RandomNumberGenerator
+{
+    private static readonly byte[] Rb = new byte[4];
+
+    public static int Next(int min, int max)
+    {
+        if (min > max)
+            throw new ArgumentOutOfRangeException(nameof(min));
+
+        return Next(max - min) + min;
+    }
+
+    public static int Next(int max)
+    {
+        if (max < 0)
+            throw new ArgumentOutOfRangeException(nameof(max));
+
+        using var rng = System.Security.Cryptography.RandomNumberGenerator.Create();
+
+        rng.GetBytes(Rb);
+
+        var value = BitConverter.ToInt32(Rb, 0);
+
+        value %= max + 1;
+
+        if (value < 0) value = -value;
+
+        return value;
+    }
+}
---
+++ src/Test/Core/Infra.Core.IntegrationTest/Utility/RandomNumberGeneratorTests.cs
@@ -0,0 +1,45 @@
+﻿using Infra.Core.Utility;
+using NUnit.Framework;
+
+namespace Infra.Core.IntegrationTest.Utility;
+
+public class RandomNumberGeneratorTests
+{
+    [Test]
+    public void RandomSuccess()
+    {
+        const int max = 10;
+        const int count = 10000;
+
+        var index = 0;
+        var pass = true;
+
+        while (index < count)
+        {
+            pass = RandomNumberGenerator.Next(max) <= max;
+            index++;
+        }
+
+        Assert.IsTrue(pass);
+    }
+
+
+    [Test]
+    public void RandomRangeSuccess()
+    {
+        const int max = 10;
+        const int min = 0;
+        const int count = 10000;
+
+        var index = 0;
+        var pass = true;
+
+        while (index < count)
+        {
+            var randomNumber = RandomNumberGenerator.Next(min, max);
+            pass = randomNumber is <= max and >= min;
+            index++;
+        }
+        Assert.IsTrue(pass);
+    }
+}
