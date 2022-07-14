1922b644fba1a36ac3941f73670629e0fba23b20
cdcd72 <cdcd71517@gmail.com>
2022-07-14 15:29:44 +0800

    初步定義 IObjectStorage

---
+++ src/Core/Infra.Core/Storage/Abstractions/IObjectStorage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Infra.Core.Storage.Abstractions
+{
+    public interface IObjectStorage
+    {
+        #region Bucket
+
+        Task<bool> BucketExistsAsync(string name);
+
+        Task MakeBucketAsync(string name);
+
+        Task RemoveBucketAsync(string name);
+
+        Task<List<string>> ListBucketsAsync();
+
+        #endregion
+
+        #region Object
+
+        Task<bool> ObjectExistsAsync(string bucketName, string objectName);
+
+        Task PutObjectAsync(string bucketName, string objectName, Stream data);
+
+        Task RemoveObjectAsync(string bucketName, string objectName);
+
+        Task<Stream> GetObjectAsync(string bucketName, string objectName);
+
+        Task<List<string>> ListObjectsAsync(string bucketName, string prefix = null, bool recursive = true);
+
+        #endregion
+
+    }
+}
