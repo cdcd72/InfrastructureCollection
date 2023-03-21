b3f095c8b6d2cafc5f403a57788d184a9f453076
cdcd72 <cdcd71517@gmail.com>
2023-03-21 17:07:39 +0800

    1. Remove GrpcFileServer Startup.cs 2. Modify GrpcFileServer log with Serilog

--- src/Server/File/GrpcFileServer/Program.cs
+++ src/Server/File/GrpcFileServer/Program.cs
@@ -1,16 +1,62 @@
-using NLog.Web;
+using GrpcFileServer.Services;
+using Infra.Core.FileAccess.Abstractions;
+using Infra.FileAccess.Physical;
+using Serilog;
+using GrpcFileServerConfig = GrpcFileServer.Configuration;
+using PhysicalFileAccessConfig = Infra.FileAccess.Physical.Configuration;
 
-namespace GrpcFileServer
+#pragma warning disable CA1852
+
+try
 {
-    public static class Program
-    {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+    var builder = WebApplication.CreateBuilder(args);
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
-                .UseNLog();
+    var config = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+        .Build();
+
+    #region Serilog related configuration
+
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(config)
+        .CreateLogger();
+
+    builder.Host.UseSerilog();
+
+    #endregion
+
+    builder.Configuration.AddConfiguration(config);
+
+    builder.Services
+        // Grpc 檔案伺服器設定
+        .Configure<GrpcFileServerConfig.Settings>(settings => builder.Configuration.GetSection(GrpcFileServerConfig.Settings.SectionName).Bind(settings))
+        // 實體檔案存取設定
+        .Configure<PhysicalFileAccessConfig.Settings>(settings => builder.Configuration.GetSection(PhysicalFileAccessConfig.Settings.SectionName).Bind(settings));
+
+    builder.Services.AddSingleton<IFileAccess, PhysicalFileAccess>();
+
+    builder.Services.AddGrpc();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
     }
+
+    app.MapGrpcService<DirectoryService>();
+    app.MapGrpcService<FileService>();
+    app.MapGet("/", async context => await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909"));
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "GrpcFileServer terminated unexpectedly.");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
 }
