521002f69f2a469cc479f8dcff8feef927265e7b
cdcd72 <cdcd71517@gmail.com>
2024-01-18 13:31:58 +0800

    [Infra.EventBus.RabbitMQ] Improve code with net8 feature

--- src/Infra/EventBus/Infra.EventBus.RabbitMQ/Configuration/Settings.cs
+++ src/Infra/EventBus/Infra.EventBus.RabbitMQ/Configuration/Settings.cs
@@ -1,17 +1,16 @@
-﻿namespace Infra.EventBus.RabbitMQ.Configuration
+﻿namespace Infra.EventBus.RabbitMQ.Configuration;
+
+public class Settings
 {
-    public class Settings
-    {
-        public const string SectionName = "EventBus:RabbitMQ";
+    public const string SectionName = "EventBus:RabbitMQ";
 
-        /// <summary>
-        /// RabbitMQ queue name
-        /// </summary>
-        public string QueueName { get; set; }
+    /// <summary>
+    /// RabbitMQ queue name
+    /// </summary>
+    public string QueueName { get; set; }
 
-        /// <summary>
-        /// RabbitMQ event publish retry count
-        /// </summary>
-        public int RetryCount { get; set; }
-    }
+    /// <summary>
+    /// RabbitMQ event publish retry count
+    /// </summary>
+    public int RetryCount { get; set; }
 }
--- src/Infra/EventBus/Infra.EventBus.RabbitMQ/Configuration/Validators/SettingsValidator.cs
+++ src/Infra/EventBus/Infra.EventBus.RabbitMQ/Configuration/Validators/SettingsValidator.cs
@@ -1,24 +1,23 @@
 ﻿#pragma warning disable CA2208
 
-namespace Infra.EventBus.RabbitMQ.Configuration.Validators
+namespace Infra.EventBus.RabbitMQ.Configuration.Validators;
+
+public static class SettingsValidator
 {
-    public static class SettingsValidator
+    public static bool TryValidate(Settings settings, out AggregateException validationExceptions)
     {
-        public static bool TryValidate(Settings settings, out AggregateException validationExceptions)
-        {
-            if (settings is null) throw new ArgumentNullException(nameof(settings));
+        ArgumentNullException.ThrowIfNull(settings);
 
-            var exceptions = new List<Exception>();
+        var exceptions = new List<Exception>();
 
-            if (string.IsNullOrWhiteSpace(settings.QueueName))
-                exceptions.Add(new ArgumentNullException(nameof(settings.QueueName)));
+        if (string.IsNullOrWhiteSpace(settings.QueueName))
+            exceptions.Add(new ArgumentNullException(nameof(settings.QueueName)));
 
-            if (settings.RetryCount < 5)
-                exceptions.Add(new ArgumentNullException(nameof(settings.RetryCount)));
+        if (settings.RetryCount < 5)
+            exceptions.Add(new ArgumentNullException(nameof(settings.RetryCount)));
 
-            validationExceptions = new AggregateException(exceptions);
+        validationExceptions = new AggregateException(exceptions);
 
-            return !exceptions.Any();
-        }
+        return exceptions.Count == 0;
     }
 }
