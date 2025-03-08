67f0d34521d2623144af110d3b8e074d920c0c9d
cdcd72 <cdcd71517@gmail.com>
2025-03-08 20:12:31 +0800

    Add some health check implements

---
+++ src/Core/Infra.Core/HealthCheck/HealthCheckHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Infra.Core.HealthCheck.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Infra.Core.HealthCheck;
+
+public static class HealthCheckHelper
+{
+    private static readonly JsonSerializerOptions JsonSerializerWriteOptions = new()
+    {
+        WriteIndented = true
+    };
+
+    public static Task CreateResponse(HttpContext httpContext, HealthReport result)
+    {
+        httpContext.Response.ContentType = "application/json";
+
+        var response = new HealthCheckResponse
+        {
+            Status = $"{result.Status}",
+            TotalDuration = $"{result.TotalDuration.TotalSeconds:0:0.00}",
+            DependencyServices = result.Entries.Select(service => new DependencyService
+            {
+                Key = service.Key,
+                Status = $"{service.Value.Status}",
+                Duration = $"{service.Value.Duration.TotalSeconds:0:0.00}",
+                Tags = string.Join(",", service.Value.Tags.ToArray()),
+                Exception = service.Value.Exception?.Message
+            }).ToArray()
+        };
+
+        return httpContext.Response.WriteAsync(JsonSerializer.Serialize(response, JsonSerializerWriteOptions));
+    }
+}
---
+++ src/Core/Infra.Core/HealthCheck/Models/HealthCheckResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Infra.Core.HealthCheck.Models;
+
+public class HealthCheckResponse
+{
+    public string Status { get; set; }
+
+    public string TotalDuration { get; set; }
+
+    public DependencyService[] DependencyServices { get; set; }
+}
+
+public class DependencyService
+{
+    public string Key { get; set; }
+
+    public string Status { get; set; }
+
+    public string Duration { get; set; }
+
+    public string Tags { get; set; }
+
+    public string Exception { get; set; }
+}
